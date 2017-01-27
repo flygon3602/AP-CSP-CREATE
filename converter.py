6f454cc0b76244937e5ce4a351c157a7a51e5cdd
flygon3602 <pokemaster3602@gmail.com>
2017-01-27 15:57:57 -0700

    made classes

--- converter.py
+++ converter.py
@@ -1,4 +1,20 @@
-class country:
+point_choice='100, 200, or, 300 point game (enter 100, 200, or 300)'
+class player:
+    def __init__(self, name, weighting, score):
+        self.name=name
+        self.weighting=weighting
+        self.score=score
 
-    def __init__(self, per_dollar):
-        self.per_dollar = per_dollar
+class category:
+    def __init__(self, name, question):
+        self.name=name
+        self.question=question
+
+class question:
+    def __init__(self,description,answer,option):
+        self.description=description
+        self.answer=answer
+        self.option=option
+player1 = player(str(input('name: ')), )
+while score<point_choice:
+    if question
