d7375bf8425ceb3a3164b8aa0cc7ef10892f6a37
lotr101 <piaws1110@gmail.com>
2017-01-27 17:09:45 -0700

    Made China category and questions

--- converter.py
+++ converter.py
@@ -18,3 +18,37 @@ class question:
 player1 = player(str(input('name: ')), )
 while score<point_choice:
     if question
+
+
+C1 = question("T/F China invented ice cream?", 'a', '''a.  True
+b.  False''')
+
+C2 = question("T/F: China uses the death penalty the most times? ", 'a', '''a.  True
+b.  False''')
+
+C3 = question('T/F China’s national sport is Table Tennis? ', 'a',  '''a.  True
+b.  False''')
+
+C4 = question('T/F China has censored the word “capitalism”?' , 'b', '''a.  True
+b.  False''' )
+
+C5 = question('T/F China gives 15 years sentence to fraudulent company executives? ' , 'b', '''a.  True
+b.  False''' )
+
+C6 = question('T/F China has banned puns in public advertisements?' , 'a', '''a.  True
+b.  False''' )
+
+C7 = question('T/F China is the most religious country in the world' , 'b', '''a.  True
+b.  False''' )
+
+C8 = question('T/F China has made it illegal to not visit one’s parents if they are over 60 every day? ' , 'a', '''a.  True
+b.  False''' )
+
+C9 = question('T/F China does not allow marriages of dead people to each other?' , 'b', '''a.  True
+b.  False''' )
+
+China = category("China", [C1,C2,C3,C4,C5,C6,C7,C8,C9])
+
+
+
+
